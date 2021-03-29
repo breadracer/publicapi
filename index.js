e28b84d7119ff8069fc3d988e30debf613ac0fe2
Shawn Ma <breadracer@g.ucla.edu>
2021-03-28 22:20:28 -0700

    Implemented a simple search upon university name, which leads to a list of matched university names. Added index.js file.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello world!")
+const express = require('express');
+const ejs = require('ejs');
+const axios = require('axios');
+
+const app = express();
+
+// app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let people = ['geddy', 'neil', 'alex'];
+let people2 = ['geddydddddddd', 'neil', 'alex'];
+
+
+app.get('/', (req, res) => {
+  res.render('index', { people });
+  
+});
+
+app.post('/search', async (req, res) => {
+  console.log(req.body);
+  let universities = await axios({
+    method: 'get',
+    url: `http://universities.hipolabs.com/search?name=${req.body.name}`
+  });
+  // console.log(universities);
+  res.render('list', { universities: universities.data });
+
+});
+
+app.listen(3000);
