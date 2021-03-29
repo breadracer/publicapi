ea02b0a4f247c28bbab22275db36cc332f700170
Shawn Ma <breadracer@g.ucla.edu>
2021-03-29 00:25:28 -0700

    Implemented basic server-side pagination.

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let people = ['geddy', 'neil', 'alex'];
-let people2 = ['geddydddddddd', 'neil', 'alex'];
 
+// Length of a page for pagination
+let page_length = 5;
 
 app.get('/', (req, res) => {
   res.render('index', { people });
@@ -19,16 +19,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/search', async (req, res) => {
-  console.log(req.param);
+  // Get the request parameters
   let name = req.param('name');
   let country = req.param('country');
+  let page = Number(req.param('page'));
+
+  // Make the API call
   let universities = await axios({
     method: 'get',
     url: `http://universities.hipolabs.com/search?name=${
       name ? name : ''}&country=${country ? country : ''}`
   });
-  console.log(`http://universities.hipolabs.com/search?name=${name ? name : ''}&country=${country ? country : ''}`);
-  res.render('list', { universities: universities.data });
+
+  // Pagination
+  let total_length = universities.data.length;
+  let start = (page - 1) * page_length;
+  let end = start + page_length;
+  if (total_length < end) end = total_length;
+  let data = universities.data.slice(start, end);
+
+  console.log(total_length)
+  // Rendering
+  res.render('list', { data, total_length, page_length, name, country, page });
 
 });
 
